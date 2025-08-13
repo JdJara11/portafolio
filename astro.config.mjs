4d6123cf27e4484b57e9bb3c12da9b1c90021bfb
Juan David Jaramillo <davidjaramillogarcia@gmail.com>
2025-08-13 09:49:18 -0500

    cambio

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://jdjara11.github.io/portafolio',
+  base: '/portafolio/'
 });
